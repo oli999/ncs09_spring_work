67b0e554d57b7a2c97198824a492bcb1aecf5083
kimgura <oli999@naver.com>
2020-01-29 11:06:13 +0900

    Spring03 프로젝트에 *.do 설정과 index 페이지 기본설정 추가

--- Spring03_Basic/src/main/java/com/gura/spring03/HomeController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/HomeController.java
@@ -1,39 +1,48 @@
 package com.gura.spring03;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
+/*
+ *  @Controller 어노테이션
+ *  - 해당 클래스가 Spring MVC  프로젝트에서 Controller 가 될수 있도록 한다.
+ *  - component scan 을 통해서 spring bean container 에서 관리가 되는 
+ *    bean 이 되어야 동작을 한다.  
  */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	// /home.do  요청이 왔을때 요청을 처리하게 하는 @RequestMapping 어노테이션
+	@RequestMapping("/home.do")
+	public String home(HttpServletRequest request) {
+		//모델
+		List<String> notice=new ArrayList<>();
+		notice.add("감기조심");
+		notice.add("코로나 조심");
+		notice.add("다들 살아 남아요~");
+		notice.add("어쩌구...");
+		notice.add("저쩌구...");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		//모델을 request 에 담는다.
+		request.setAttribute("notice", notice);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		/*
+		 *  "home" 을 리턴해주면
+		 *  
+		 *  "/WEB-INF/views/"+"home"+".jsp" 와 같은 문자열이 만들어 지고
+		 *  
+		 *  /WEB-INF/views/home.jsp 페이지로 forward 이동 되어서 
+		 *  
+		 *  응답된다. 
+		 */
 		return "home";
 	}
-	
 }
+
+
+
--- Spring03_Basic/src/main/webapp/WEB-INF/views/home.jsp
+++ Spring03_Basic/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,25 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<title>/views/home.jsp</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+<h1>인덱스 페이지 입니다</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<h2>공지사항</h2>
+<ul>
+	<c:forEach var="tmp" items="${notice }">
+		<li>${tmp }</li>
+	</c:forEach>
+</ul>
 </body>
 </html>
+
+
+
+
+
