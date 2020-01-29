129e9be8928f88e5c05ccfcba775655e87451a17
kimgura <oli999@naver.com>
2020-01-29 15:15:20 +0900

    Spring03 프로젝트에 로그인 요청 처리 2번째 방법 구현함

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
@@ -36,6 +38,28 @@ public class UsersController {
 		request.setAttribute("isSuccess", isSuccess);
 		//view page 의 정보를 리턴한다.
 		return "users/login";
+	}
+	/*
+	 *  @RequestParam 어노테이션은 전송되는 파라미터를 자동으로 추출할때 사용한다.
+	 *  단, 지역 변수의 이름은 파라미터의 이름과 같아야 한다. 
+	 */
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, 
+			@RequestParam String pwd, HttpSession session,
+			ModelAndView mView) {
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 한다
+			session.setAttribute("id", id);
+		}
+		//view 페이지에서 필요한 모델을 담고 
+		mView.addObject("isSuccess", isSuccess);
+		//view page 의 정보도 담고 
+		mView.setViewName("users/login");
+		//리턴해 준다. 
+		return mView;
 	}
 }
 
--- Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
+++ Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
@@ -7,8 +7,16 @@
 <title>/users/loginform.jsp</title>
 </head>
 <body>
-<h1>로그인 폼 입니다.</h1>
+<h1>로그인 폼 입니다. 1</h1>
 <form action="login.do" method="post">
+	<label for="id">아이디</label>
+	<input type="text" name="id" id="id"/>
+	<label for="pwd">비밀번호</label>
+	<input type="password" name="pwd" id="pwd"/>
+	<button type="submit">로그인</button>
+</form>
+<h1>로그인 폼 입니다. 2</h1>
+<form action="${pageContext.request.contextPath }/users/login2.do" method="post">
 	<label for="id">아이디</label>
 	<input type="text" name="id" id="id"/>
 	<label for="pwd">비밀번호</label>
