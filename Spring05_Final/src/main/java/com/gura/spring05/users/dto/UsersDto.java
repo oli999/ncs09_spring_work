6537aadfdcb7d8fae69636dd9490104336f73b30
kimgura <oli999@naver.com>
2020-02-03 12:00:13 +0900

    [Spring05] Users 에 관련해서 mybatis 사용할 준비함

---
+++ Spring05_Final/src/main/java/com/gura/spring05/users/dto/UsersDto.java
@@ -0,0 +1,56 @@
+package com.gura.spring05.users.dto;
+
+public class UsersDto {
+	private String id;
+	private String pwd;
+	private String email;
+	private String regdate;
+	private String profile; //프로필 이미지 경로를 저장할 필드
+	//디폴트 생성자 
+	public UsersDto() {}
+	public UsersDto(String id, String pwd, String email, String regdate, String profile) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+		this.email = email;
+		this.regdate = regdate;
+		this.profile = profile;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+}
+
+
+
+
+
+
