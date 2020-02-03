503ff5838f70420b8ece8fac7c87a8ca0fd4dc74
kimgura <oli999@naver.com>
2020-02-03 19:10:36 +0900

    [Spring05] 회원 가입할때 비밀번호를 암호화 해서 저장하도록 수정함

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gura.spring05.users.dao.UsersDao;
@@ -24,10 +25,23 @@ public class UsersServiceImpl implements UsersService{
 	}
 	@Override
 	public void addUser(UsersDto dto) {
+		//비밀번호를 암호화 한다.
+		String encodedPwd=new BCryptPasswordEncoder().encode(dto.getPwd());
+		//암호화된 비밀번호를 UsersDto 에 다시 넣어준다.
+		dto.setPwd(encodedPwd);
+		//UsersDao 객체를 이용해서 DB 에 저장하기 
 		dao.insert(dto);
 	}
 
 }
+
+
+
+
+
+
+
+
 
 
 
