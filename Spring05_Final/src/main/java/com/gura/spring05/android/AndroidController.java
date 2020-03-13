cbe91b7a5b2e173f95fdd2a087122686c0a97518
kimgura <oli999@naver.com>
2020-03-13 11:25:24 +0900

    AndroidController 추가함

---
+++ Spring05_Final/src/main/java/com/gura/spring05/android/AndroidController.java
@@ -0,0 +1,46 @@
+package com.gura.spring05.android;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AndroidController {
+	/*
+	 * 	["김구라","해골","원숭이"] 형식의 문자열이 응답되는 메소드 
+	 */
+	@ResponseBody
+	@RequestMapping("/android/getnames")
+	public List<String> getNames(){
+		List<String> names=new ArrayList<String>();
+		names.add("김구라");
+		names.add("해골");
+		names.add("원숭이");
+		return names;
+	}
+	/*
+	 *  {"num":1,"name":"김구라","isMan":true} 형식의 
+	 *  json 문자열이 응답된다. 
+	 */
+	@ResponseBody
+	@RequestMapping("/android/getdetail")
+	public Map<String, Object> getDetail(){
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("num", 1);
+		map.put("name", "김구라");
+		map.put("isMan", true);
+		return map;
+	}
+}
+
+
+
+
+
+
+
